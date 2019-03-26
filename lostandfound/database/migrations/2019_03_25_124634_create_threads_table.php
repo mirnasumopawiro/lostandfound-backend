5e5bfc2ee2312b7ac2de84fbbec1297f12658739
Mirna Sumopawiro <mirna.brighton@gmail.com>
2019-03-26 11:26:12 +0700

    database columns done

--- lostandfound/database/migrations/2019_03_25_124634_create_threads_table.php
+++ lostandfound/database/migrations/2019_03_25_124634_create_threads_table.php
@@ -15,7 +15,15 @@ class CreateThreadsTable extends Migration
     {
         Schema::create('threads', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('users_id')->unsigned();
+            $table->boolean('is_found')->default(0);
+            $table->string('title');
+            $table->string('description');
+            $table->string('photo');
+            $table->string('location');
+            $table->time('time');
             $table->timestamps();
+            $table->foreign('users_id')->references('id')->on('users');
         });
     }
 
